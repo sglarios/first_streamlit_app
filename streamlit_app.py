1728e50a315ed829341671b74e2aa5c0c27e8365
sglarios <104995054+sglarios@users.noreply.github.com>
2022-10-05 14:00:19 +0200

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 import streamlit
+import pandas
+
+my_fruit_list=read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title('My parents new healthy diner')
 streamlit.header('blabla menu')
 
 streamlit.text('blablabla')
-treamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.dataframe(my_fruit_list)
